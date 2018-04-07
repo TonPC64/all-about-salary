3b0f261283bc9a57a257348734a6b64a87a239d5
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-07 23:56:22 +0700

    use begeta

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="">
+  <div class="dp-flex f-drt-column">
     <h1 class="title has-text-centered">All About Salary</h1>
-    <router-link class="button is-primary is-large" :to="{name: 'countdown'}">Countdown</router-link>
-    <router-link class="button is-success is-large" disabled :to="{name: 'todo'}">Todolist(WIP)</router-link>    
+    <router-link class="button is-primary is-large mg-bt-8px" :to="{name: 'countdown'}">Countdown</router-link>
+    <router-link class="button is-success is-large mg-bt-8px" disabled :to="{name: 'todo'}">Month Todolist(WIP)</router-link>    
   </div>
 </template>
