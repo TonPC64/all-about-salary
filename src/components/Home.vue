4da976698c726cb4a0aa95ae19d1e5c0e42ca963
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-08 23:44:03 +0700

    menupage middle page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dp-flex f-drt-column">
+  <div class="dp-flex f-drt-column jtf-ct-center h-10calc">
     <h1 class="title has-text-centered">All About Salary</h1>
     <router-link class="button is-primary is-large mg-bt-8px" :to="{name: 'countdown'}">Countdown</router-link>
     <router-link class="button is-success is-large mg-bt-8px" disabled :to="{name: 'todo'}">Month Todolist(WIP)</router-link>    
