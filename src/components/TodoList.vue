09c50fbfa9a1077e015ceeb4dab29bb749581134
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-11 23:30:53 +0700

    add scritp

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,5 +2,17 @@
   <div>
     <span>TODOLIST</span>
     <span>(AFTER SALARY IN)</span>
+    <div></div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      todo: []
+    }
+  }
+}
+</script>
+
