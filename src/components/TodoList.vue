0c17eaa03ff154170a00a696d31412626f0ba95f
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-14 23:05:32 +0700

    padding in list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,12 +3,12 @@
     <div class="dp-flex al-it-center f-drt-column">
       <div class="menu">TODOLIST</div>
       <div class="sub-menu">(AFTER SALARY COME)</div>
-      <div class="dp-flex al-it-flex-start w-100pct">
+      <div class="dp-flex al-it-flex-start w-100pct pd-5px">
         <b-checkbox :key="index" v-for="(list, index) in todo">{{list.list}} : {{list.price}}</b-checkbox>
       </div>
       <div>
         <b-field>
-            <b-input class="mg-5px " v-model="list" placeholder="Add List"></b-input>
+            <b-input class="mg-5px" v-model="list" placeholder="Add List"></b-input>
             <b-input class="mg-5px w-30pct" v-model="price" type="number" placeholder="Price"></b-input>
             <button class="button is-primary mg-5px" @click="addToList(list, price)">Add</button>
         </b-field>
