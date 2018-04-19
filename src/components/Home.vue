7f8f9b1e85063e5421c8a6dc3ba4ce324355d79a
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-19 21:27:36 +0700

    style setting page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="dp-flex f-drt-column jtf-ct-center h-10calc pd-bt-70px">
-    <h1 class="title has-text-centered">All About Salary</h1>
+  <div class="dp-flex f-drt-column jtf-ct-center h-10calc pd-bt-70px app bg">
+    <h1 class="title has-text-centered f-w-600 is-uppercase">All About Salary</h1>
     <router-link class="button is-primary is-large mg-bt-8px" :to="{name: 'countdown'}">Countdown</router-link>
-    <router-link class="button is-success is-large mg-bt-8px" disabled :to="{name: 'todo'}">Month Todolist(WIP)</router-link>    
-    <router-link class="button is-info is-large mg-bt-8px" :to="{name: 'setting'}">Setting</router-link>    
+    <router-link class="button is-success is-large mg-bt-8px" disabled :to="{name: 'todo'}">Month Todolist</router-link>    
+    <router-link class="button is-info is-large mg-bt-8px" disabled :to="{name: 'setting'}">Setting</router-link>    
   </div>
 </template>
+
+<style scoped>
+.bg {
+  background: linear-gradient(to bottom, #ffafbd, #ffc3a0);
+}
+</style>
--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>setting</div>
+  <div class="app bg dp-flex f-drt-column al-it-center">
+    <span class="cl-white is-uppercase f-w-600 is-size-3">Setting</span>
+  </div>
 </template>
+
+<style scoped>
+.bg {
+  background: linear-gradient(to bottom, #485563, #29323c);
+}
+</style>
